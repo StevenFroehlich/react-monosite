4e98c5082dd42a4f139c02812edfbda14622fab5
Steven Fröhlich <steven.froehlich@valtech.com>
2021-10-19 12:55:13 +0200

    setup sections width snappy scroll effect

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import "./portfolio.scss";
 
-export default function portfolio() {
+export default function Portfolio() {
     return (
         <div className="portfolio">
             
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import "./works.scss";
 
 export default function Works() {
     return (
